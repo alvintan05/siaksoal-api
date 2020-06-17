b9c0aad542d1c6f075d141132c2ed55b7662d2ff
musyaffanq <50899323+musyaffanq@users.noreply.github.com>
2020-06-17 22:41:16 +0700

    Update Kps.php

--- application/controllers/api/Kps.php
+++ application/controllers/api/Kps.php
@@ -15,14 +15,14 @@ class Kps extends REST_Controller
 		$this->load->model('Model_kps', 'mkp');
 	}
 	
-	public function search_post()
+	public function search_get()
 	{   
         $responseData = null;
         // masih belum selesai
 		$search = array(			
-			'tahun' => $this->input->post('tahun'),
-			'semester' => $this->input->post('semester'),
-			'jenisSoal'	=> $this->input->post('jenissoal')
+			'tahun' => $this->get('tahun'),
+			'semester' => $this->get('semester'),
+			'jenisSoal'	=> $this->get('jenissoal')
         );
         
 		$data = $this->mkp->getSearch($search);
